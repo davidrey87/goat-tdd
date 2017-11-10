164cfa08e8763b2d982b5fb3fc84f6cbae458628
David REyes <isc.redh@gmail.com>
2017-11-10 00:46:33 -0600

    Fix

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -116,6 +116,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Satisfied, they both go back to sleep
 
     def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
         # She starts a new list and sees the input is nicely
         # centered there too
         inputbox.send_keys('testing')
@@ -125,5 +137,5 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=5
         )
